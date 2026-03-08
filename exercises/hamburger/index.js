a9162e491011de484e51e6b5e8d2efb913e05633
Artem Mehes <artemmeges@gmail.com>
2026-03-08 09:56:42 +0200

    feat: Implement hamburger menu animation with a flatten effect on pointerdown and toggle between hamburger and 'X' on click.

--- exercises/hamburger/index.js
+++ exercises/hamburger/index.js
@@ -1,50 +1,81 @@
-import { animate } from "motion";
+import { animate } from 'motion';
 
-const button = document.querySelector("button");
-const _line1 = button.querySelector("line:first-child");
-const _line2 = button.querySelector("line:last-child");
+const button = document.querySelector('button');
+const line1 = button.querySelector('line:first-child');
+const line2 = button.querySelector('line:last-child');
+
+const VIEWBOX_SIZE = 24;
+const INSET_X_BY = 6;
 
 let isMenuOpen = false;
 
-function handleClick() {
-	isMenuOpen = !isMenuOpen;
+function handleFlatten() {
+  if (checkPrefersReducedMotion()) {
+    return;
+  }
 
-	const prefersReducedMotion = _checkPrefersReducedMotion();
+  // Very quick duration, since there usually isn't
+  // much time between press and release:
+  const duration = 0.1;
 
-	const options = {
-		duration: prefersReducedMotion ? 0 : undefined,
-	}
-
-	animate(_line1, {
-		y1: isMenuOpen ? 6 : 8,
-		y2: isMenuOpen ? 18 : 8,
-	}, options);
-
-	animate(_line2, {
-		y1: isMenuOpen ? 18 : 16,
-		y2: isMenuOpen ? 6 : 16,
-	}, options);
-}
-
-button.addEventListener("click", handleClick);
-
-function _checkPrefersReducedMotion() {
-	return !window.matchMedia("(prefers-reduced-motion: no-preference)").matches;
-}
-
-/*
-  Motion Cheatsheet:
-  
-  animate() is the main function we’ll be using.
-  It takes two arguments:
-  
-  1. The element to animate
-  2. An object of the properties/values to change.
-  
-  For example:
-  
+  // Animate into a single flat line, regardless of
+  // whether it's currently an X or =:
+  [line1, line2].forEach((elem) => {
+    const lineInset = 5;
     animate(
-      button,
-      { opacity: 0 }
+      elem,
+      {
+        x1: lineInset,
+        y1: VIEWBOX_SIZE * 0.5,
+        x2: VIEWBOX_SIZE - lineInset,
+        y2: VIEWBOX_SIZE * 0.5,
+      },
+      { duration }
     );
-*/
+  });
+}
+
+// ⚠️ This function is totally unchanged:
+function handleClick() {
+  const prefersReducedMotion = checkPrefersReducedMotion();
+  const duration = prefersReducedMotion ? 0 : 0.3;
+
+  isMenuOpen = !isMenuOpen;
+
+  if (isMenuOpen) {
+    animate(line1, {
+      x1: INSET_X_BY,
+      y1: INSET_X_BY,
+      x2: VIEWBOX_SIZE - INSET_X_BY,
+      y2: VIEWBOX_SIZE - INSET_X_BY,
+    }, { duration });
+    animate(line2, {
+      x1: INSET_X_BY,
+      y1: VIEWBOX_SIZE - INSET_X_BY,
+      x2: VIEWBOX_SIZE - INSET_X_BY,
+      y2: INSET_X_BY,
+    }, { duration });
+  } else {
+    animate(line1, {
+      x1: 5,
+      y1: 8,
+      x2: 19,
+      y2: 8,
+    }, { duration });
+    animate(line2, {
+      x1: 5,
+      y1: 16,
+      x2: 19,
+      y2: 16,
+    }, { duration });
+  }
+}
+
+button.addEventListener('pointerdown', handleFlatten);
+button.addEventListener('click', handleClick);
+
+function checkPrefersReducedMotion() {
+  return !window.matchMedia(
+    '(prefers-reduced-motion: no-preference)'
+  ).matches;
+}
