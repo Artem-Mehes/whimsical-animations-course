a5a4d1e6784b927c0d0853df7a811c5e195f7e4f
Artem Mehes <artemmeges@gmail.com>
2026-03-09 13:42:47 +0200

    feat: Implement a circular progress indicator with an interactive slider control.

--- exercises/circular-progress-indicator/index.js
+++ exercises/circular-progress-indicator/index.js
@@ -4,28 +4,22 @@ const slider = document.querySelector("#progressSlider");
 const bigNumber = document.querySelector(".bigNumber");
 const progressIndicator = document.querySelector(".progressIndicator");
 
-const radius = progressIndicator.getAttribute("r");
-const circumference = Math.ceil(2 * Math.PI * radius);
-progressIndicator.style.strokeDasharray = `${circumference}px, 1000px`;
-progressIndicator.style.strokeDashoffset = `${circumference}px`;
-
 function handleChange(ev) {
 	const progress = Number(ev.target.value);
 
-	const circleNumber = normalize(
+	bigNumber.innerText = progress + "%";
+
+	progressIndicator.style.strokeDashoffset = normalize(
 		progress,
 		{
 			min: 0,
 			max: 100,
 		},
 		{
-			min: 0,
-			max: +circumference,
+			min: 100,
+			max: 0,
 		},
 	);
-	progressIndicator.style.strokeDashoffset = `${circleNumber}px`;
-
-	bigNumber.innerText = progress + "%";
 }
 
 slider.addEventListener("input", handleChange);
